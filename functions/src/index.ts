c699db39cf0af80634b2c984b1bfda5ebaf3537d
飯盛正慶 <masamichhhi@gmail.com>
2021-07-23 16:09:35 +0900

    add todo crud

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -28,12 +28,63 @@ export const getUsers = functions.https.onRequest(async (request, response) => {
   }
 });
 
+export const updateUser = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method == "POST") {
+      if (!request.body.userId) {
+        response.status(400).send("userId should be passed in parameter");
+        return;
+      }
+
+      const updateUserResponse = await prisma.user.update({
+        where: {
+          id: request.body.userId,
+        },
+        data: {
+          email: request.body.email,
+          name: request.body.name,
+        },
+      });
+      response.status(200).send(updateUserResponse);
+    } else {
+      response.status(403).send("forbidden!");
+    }
+  }
+);
+
+export const deleteUser = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method == "POST") {
+      if (!request.body.userId) {
+        response.status(400).send("userId should be passed in parameter");
+        return;
+      }
+      const deleteUserResponse = await prisma.user.delete({
+        where: {
+          id: Number(request.body.userId),
+        },
+      });
+      response.status(200).send(deleteUserResponse);
+    } else {
+      response.status(403).send("forbidden!");
+    }
+  }
+);
+
 export const createTodo = functions.https.onRequest(
   async (request, response) => {
     if (request.method == "POST") {
+      if (
+        !request.body.deadline ||
+        !request.body.title ||
+        !request.body.userId
+      ) {
+        response.status(400).send("bad request");
+        return;
+      }
       await prisma.todo.create({
         data: {
-          deadline: request.body.deadline,
+          deadline: new Date(request.body.deadline),
           title: request.body.title,
           assignee: {
             connect: {
@@ -43,6 +94,65 @@ export const createTodo = functions.https.onRequest(
         },
       });
       response.status(200).send("success!");
+    } else {
+      response.status(403).send("forbidden!");
+    }
+  }
+);
+
+export const getTodos = functions.https.onRequest(async (request, response) => {
+  if (request.method == "GET") {
+    const getTodosResponse = await prisma.todo.findMany({
+      where: {
+        assigneeId: request.params.userId
+          ? Number(request.params.userId)
+          : undefined,
+      },
+    });
+    response.status(200).send(getTodosResponse);
+  } else {
+    response.status(403).send("forbidden!");
+  }
+});
+
+export const updateTodo = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method == "POST") {
+      if (!request.body.todoId) {
+        response.status(400).send("todoId should be passed in parameter");
+        return;
+      }
+
+      const updateUserResponse = await prisma.todo.update({
+        where: {
+          id: request.body.todoId,
+        },
+        data: {
+          title: request.body.title,
+          deadline: new Date(request.body.deadline),
+          done: request.body.done === "true",
+        },
+      });
+      response.status(200).send(updateUserResponse);
+    } else {
+      response.status(403).send("forbidden!");
+    }
+  }
+);
+
+export const deleteTodo = functions.https.onRequest(
+  async (request, response) => {
+    if (request.method == "POST") {
+      if (!request.body.todoId) {
+        response.status(400).send("todoId should be passed in parameter");
+        return;
+      }
+      const deleteTodoResponse = await prisma.todo.delete({
+        where: {
+          id: Number(request.body.todoId),
+        },
+      });
+      response.status(200).send(deleteTodoResponse);
     } else {
       response.status(403).send("forbidden!");
     }
